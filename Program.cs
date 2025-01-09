a4e3c264cde347cae70e9f034ae9b41bf1b902f1
MGHM97 <mateusoliveira469@gmail.com>
2025-01-09 14:22:07 -0400

    app main menu

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace csharpTextEditor {
+  class Program {
+    static void Main(string[] args) {
+      Menu();
+    }
+
+    static void Menu() {
+      Console.Clear();
+      Console.WriteLine("What would you like to do?");
+      Console.WriteLine("");
+      Console.WriteLine("1 -> Open file");
+      Console.WriteLine("2 -> Create new file");
+      Console.WriteLine("3 -> Exit app");
+
+      Console.WriteLine("----------/--------/--------");
+
+      Console.WriteLine("Select one of the options above: ");
+
+      short selectedOption = short.Parse(Console.ReadLine());
+
+      switch (selectedOption) {
+        case 1:  OpenFile(); break;
+        case 2: CreateFile(); break;
+        case 3: Environment.Exit(0); break;
+      }
+    }
+
+    static void OpenFile() {
+
+    }
+
+    static void CreateFile() {
+
+    }
+  }
+}
